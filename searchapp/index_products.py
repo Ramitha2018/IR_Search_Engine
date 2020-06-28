e2f86883175623e1dcf595a10b957abfb59f070d
Ramitha2018 <36252624+Ramitha2018@users.noreply.github.com>
2020-06-28 22:43:28 +0530

    Update index_products.py

--- searchapp/index_products.py
+++ searchapp/index_products.py
@@ -1,5 +1,5 @@
 from elasticsearch import Elasticsearch, helpers
-from sinling import SinhalaTokenizer
+from sinling import SinhalaTokenizer #from https://github.com/ysenarath/sinling
 from searchapp.constants import INDEX_NAME
 from searchapp.data import all_songs, SongData
 
